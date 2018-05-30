9276b3c10d587b12b97b5c27cdddb3f7d39e514a
Waner <Waner@WANER-PC>
2018-05-30 11:47:42 +0800

    5/30

--- Guitar/Controllers/MusicScoreController.cs
+++ Guitar/Controllers/MusicScoreController.cs
@@ -220,7 +220,7 @@ namespace Guitar.Controllers
         #region 评论乐谱
         [HttpPost]
         //[Login]
-        public ActionResult Comment(MusicScoreComment msc, MusicScoreStatistics mss)
+        public string Comment(MusicScoreComment msc, MusicScoreStatistics mss)
         {
             string pingluntextarea = Request["pingluntextarea"];
             int Ms_id = Convert.ToInt32(Request["Ms_id"]);
@@ -232,12 +232,14 @@ namespace Guitar.Controllers
                 msc.Addtime = System.DateTime.Now;
                 db.MusicScoreComment.Add(msc);
                 db.SaveChanges();
-                return Content("<script>;alert('评论成功!');history.go(-1)</script>");
+                //return Content("<script>;alert('评论成功!');history.go(-1)</script>");
                 //return RedirectToAction("Display", "MusicScore");
+                return "aa";
             }
             else {
                 //return Content("bb");
-                return RedirectToAction("Display", "MusicScore");
+                //return RedirectToAction("Display", "MusicScore");
+                return "ba";
             }
 
             //return RedirectToAction("Display", "MusicScore");
@@ -331,18 +333,35 @@ namespace Guitar.Controllers
             return RedirectToAction("Display", "MusicScore");
         }
         #endregion
-        public IEnumerable<MusicScoreComment> GetComment(int Ms_id) 
-        {
-            var psr = from po in db.MusicScoreComment
-                      where po.Ms_id== Ms_id
-                      orderby po.Addtime descending
-                      select po;
-            return psr;
-        }
+        //public IEnumerable<MusicCommentReplyViewModel> MsReply(int Ms_id) 
+        //{
+        //    var se = TempData["Ms_idd"];
+        //    int Ms_id1 = Convert.ToInt32(se);
+        //    var msc = from m in db.MusicScoreComment.Where(p => p.Ms_id == Ms_id1).OrderByDescending(p => p.Addtime) select m;
+        //    var msr = (from n in db.MusicScoreReply
+        //               join m in msc on n.Ms_commentid equals m.Ms_commentid
+        //               join q in db.Users on n.User_id equals q.User_id
+        //               select new MusicCommentReplyViewModel
+        //               {
+        //                   Ms_replyid = n.Ms_replyid,
+        //                   Ms_commentid = m.Ms_commentid,
+        //                   content = n.content,
+        //                   Addtime = n.Addtime,
+        //                   Ms_id = m.Ms_id,
+        //                   User_id = n.User_id,
+        //                   User_name = q.User_name,
+        //                   User_img = q.User_img
+        //               });
+        //    var reply = (from m in msr.OrderByDescending(p => p.Addtime) select m);
+        //    return reply;
+        //}
         #region 回复显示
-        public ActionResult _PartialPage2()
+        [ChildActionOnly]
+        public ActionResult MsReply(int param1, int md = 1)
         {
+            //var mdd = ViewData["md"];
             var se = TempData["Ms_idd"];
+            //int mm = param1;
             int Ms_id1 = Convert.ToInt32(se);
             var msc = from m in db.MusicScoreComment.Where(p => p.Ms_id == Ms_id1).OrderByDescending(p => p.Addtime) select m;
             var msr = (from n in db.MusicScoreReply
@@ -359,8 +378,8 @@ namespace Guitar.Controllers
                            User_name = q.User_name,
                            User_img = q.User_img
                        });
-            var reply = (from m in msr.OrderByDescending(p => p.Addtime) select m);
-            return View(reply);
+            var reply = (from m in msr.Where(p=>p.Ms_commentid== param1).OrderByDescending(p => p.Addtime) select m);
+            return PartialView(reply.ToPagedList(md,5));
         }
         #endregion
         #region 评论展示
